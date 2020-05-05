37bbfc91d3ef5c59920e1ddc89fcb9b9b803ebd0
Ali Soume'e <alidadar7676@gmail.com>
2020-05-05 06:39:02 +0430

    Refactor configmap_controller based on new utils

--- apis/ai/v1/configmap_types.go
+++ apis/ai/v1/configmap_types.go
@@ -2,6 +2,7 @@ package v1
 
 type ConfigMap struct {
 	Name   string `json:"name"`
-	Bucket string `json:"bucket"`
-	Key    string `json:"key"`
+	Bucket string `json:"bucket,omitempty"`
+	Key    string `json:"key,omitempty"`
+	Data   string `json:"data,omitempty"`
 }
--- controllers/ai/configmap_controller.go
+++ controllers/ai/configmap_controller.go
@@ -1,50 +1,29 @@
 package ai
 
 import (
-	"time"
-
 	"github.com/go-logr/logr"
-	cache "github.com/patrickmn/go-cache"
 	aiv1 "gitlab.com/Syfract/Xerac/hub/apis/ai/v1"
-	"gitlab.com/Syfract/Xerac/hub/utils/deployer"
-	env "gitlab.com/Syfract/Xerac/hub/utils/environment"
+	"gitlab.com/Syfract/Xerac/hub/utils/cache"
 	"gitlab.com/Syfract/Xerac/hub/utils/name"
 	"gitlab.com/Syfract/Xerac/hub/utils/storage"
-	batch "k8s.io/api/batch/v1"
-	core "k8s.io/api/core/v1"
-	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 type ConfigMapReconciler struct {
-	client.Client
-	log      logr.Logger
-	scheme   *runtime.Scheme
-	deployer *deployer.Deployer
-	storage  storage.Storage
-	cache    *cache.Cache
+	log logr.Logger
 }
 
-func NewConfigMapReconciler(mgr manager.Manager, log logr.Logger) (*ConfigMapReconciler, error) {
-	storage, err := storage.NewMock()
-	if err != nil {
-		return nil, err
-	}
-
+func NewConfigMapReconciler(log logr.Logger) (*ConfigMapReconciler, error) {
 	return &ConfigMapReconciler{
-		Client:   mgr.GetClient(),
-		deployer: deployer.NewDeployer(mgr.GetClient()),
-		scheme:   mgr.GetScheme(),
-		log:      log,
-		storage:  storage,
-		cache:    cache.New(time.Minute*10, time.Minute*20),
+		log: log,
 	}, nil
 }
 
-func (r *ConfigMapReconciler) Reconcile(room aiv1.Room, job *batch.Job) error {
-	err := r.reconcileConfigMaps(room)
+func (r *ConfigMapReconciler) Reconcile(src, dst *aiv1.Room) error {
+	if dst.Spec.ConfigMaps == nil {
+		dst.Spec.ConfigMaps = make([]aiv1.ConfigMap, 0)
+	}
+
+	err := r.reconcileConfigMaps(src, dst)
 	if err != nil {
 		return err
 	}
@@ -52,61 +31,29 @@ func (r *ConfigMapReconciler) Reconcile(room aiv1.Room, job *batch.Job) error {
 	return nil
 }
 
-func (r *ConfigMapReconciler) reconcileConfigMaps(room aiv1.Room) error {
-	for _, aiConfigMap := range room.Spec.ConfigMaps {
-		err := r.reconcileConfigMap(aiConfigMap)
+func (r *ConfigMapReconciler) reconcileConfigMaps(src, dst *aiv1.Room) error {
+	for _, cm := range src.Spec.ConfigMaps {
+
+		if cm.Data != "" {
+			continue
+		}
+
+		name := name.ConfigMapName(cm.Bucket, cm.Name)
+		data, err := cache.GetYamlString(name)
 		if err != nil {
-			return err
+			data, err = storage.Get(cm.Bucket, cm.Key)
+			if err != nil {
+				return err
+			}
+			cache.SetYamlString(name, data)
 		}
+
+		dst.Spec.ConfigMaps = append(dst.Spec.ConfigMaps, aiv1.ConfigMap{
+			Name:   cm.Name,
+			Bucket: cm.Bucket,
+			Key:    cm.Key,
+			Data:   data,
+		})
 	}
 	return nil
 }
-
-func (r *ConfigMapReconciler) reconcileConfigMap(aiConfigMap aiv1.ConfigMap) error {
-	data, err := r.reconcileData(aiConfigMap.Bucket, aiConfigMap.Key)
-	if err != nil {
-		return err
-	}
-
-	coreConfigMap := &core.ConfigMap{
-		ObjectMeta: meta.ObjectMeta{
-			Name:      aiConfigMap.Name,
-			Namespace: env.Namespace(),
-		},
-		Data: map[string]string{
-			"data": data,
-		},
-	}
-
-	return r.deployer.SyncConfigMap(coreConfigMap)
-}
-
-func (r *ConfigMapReconciler) reconcileData(bucket, key string) (string, error) {
-	name := name.BucketDashKey(bucket, key)
-
-	data, exists := r.cache.Get(name)
-	if exists {
-		str, ok := data.(string)
-		if ok {
-			return str, nil
-		}
-	}
-
-	str, err := r.storage.GetConfigYamlToString(bucket, key)
-	if err != nil {
-		return "", err
-	}
-	r.cache.Set(name, str, cache.DefaultExpiration)
-	return str, nil
-}
-
-// func (r *ConfigMapReconciler) reconcileOwnership(room aiv1.Room, coreConfigMap *core.ConfigMap) error {
-// 	err := controllerutil.SetControllerReference(&room, coreConfigMap, r.scheme)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	ctx, cancel := context.WithTimeout(context.Background(), env.APICallTimeout)
-// 	defer cancel()
-// 	return r.Update(ctx, coreConfigMap)
-// }
