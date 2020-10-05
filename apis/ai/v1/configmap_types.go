a976cb080d0377c29eaa2998aa9db0bd04fec093
Ali Soume'e <alidadar7676@gmail.com>
2020-10-05 04:38:05 +0330

    Remove old project

--- apis/ai/v1/configmap_types.go
+++
@@ -1,8 +0,0 @@
-package v1
-
-type ConfigMap struct {
-	Name   string `json:"name"`
-	Bucket string `json:"bucket,omitempty"`
-	Key    string `json:"key,omitempty"`
-	Data   string `json:"data,omitempty"`
-}
